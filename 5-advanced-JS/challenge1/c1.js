09bb587582081a39a146170a08141c7e5ab7a533
Seongjin <seongjinkim1123@gmail.com>
2020-01-23 16:05:30 +0900

    20200123 | 5_challenge complete

--- 5-advanced-JS/challenge1/c1.js
+++ 5-advanced-JS/challenge1/c1.js
@@ -20,9 +20,12 @@
 
 var contents = document.querySelectorAll('.content');
 var mainBtns = document.querySelectorAll('.mainBtn');
+var backArrow = document.querySelector('#backArrow');
+var forwardArrow = document.querySelector('#forwardArrow');
 var quizzes = [];
 var activeBtn;
 var storage = window.localStorage;
+var currentQuiz;
 
 init();
 
@@ -34,60 +37,151 @@ function Question(question, answers, correctAnswer){  // describe a question ( f
 };
 var questions = [];
 Question.prototype.showQuiz = function(){
-    setTextContext('#quizNumShow', ' '+this.quizNum);
-    setTextContext('#questionShow', ''+this.question);
-    setTextContext('#answer1Show', this.answers[0]);
-    setTextContext('#answer2Show', this.answers[1]);
+    currentQuiz = this;
+    setTextContext('.quizNumShow', ' '+this.quizNum);
+    setTextContext('.questionShow', ''+this.question);
+    setTextContext('.answer1Show', this.answers[0]);
+    setTextContext('.answer2Show', this.answers[1]);
+    setTextContext('.correctAnswerShow', this.correctAnswer);
 }
-Question.prototype.checkTheAnswer = function(number){
-    
+Question.prototype.checkTheAnswer = function(userAnswer){
+    if(this.correctAnswer === userAnswer){
+        addGreenBtn(document.querySelector('#answerBtn'));
+        removeRedBtn(document.querySelector('#answerBtn'));
+        setAnswerBtn('green')();
+        setValue('#answerBtn', '정답');
+    }else{
+        addRedBtn(document.querySelector('#answerBtn'));
+        removeGreenBtn(document.querySelector('#answerBtn'));
+        setAnswerBtn('red')();
+        setValue('#answerBtn', '오답');
+    }
 }
 
 // closure 사용
-function getARandomQuiz(){
-    var quizNum = Math.floor(Math.random()*quizzes.length)+1;
-    return function(){
-        if(!document.querySelector('#btn-2').classList.contains('activeBtn') && quizzes.length != 0){
-            quizzes[quizNum-1].showQuiz();
+function getAQuiz(sequence){
+    return function(quizNum){
+        if(sequence === 'random'){
+            resetRandomQuiz();
+            if(!document.querySelector('#btn-2').classList.contains('activeBtn') && quizzes.length != 0){
+                var randomNum = Math.floor(Math.random()*quizzes.length)+1;
+                quizzes[randomNum-1].showQuiz();
+            }
+        }else if(sequence === 'inOrder'){
+            if(quizzes.length != 0){
+                quizzes[quizNum-1].showQuiz();
+                checkArrows();
+            }
         }
     }
 }
+
+
+var getARandomQuiz = getAQuiz('random');
+var getTheNumOfQuiz = getAQuiz('inOrder');
+
+function setAnswerBtn(flag){
+    var answerBtn = document.querySelector('#answerBtn');
+    return function(){
+        if(flag === 'red'){
+            answerBtn.onclick = openPrompt;
+            answerBtn.style.cursor='pointer';
+        }else if(flag === 'green'){
+            answerBtn.onclick = '';
+            answerBtn.style.cursor='default';
+        }else if(flag === 'reset'){
+            answerBtn.onclick = openPrompt;
+            answerBtn.style.cursor='pointer';
+        }
+    }
+}
+
+function checkArrows(){
+    /*
+        양쪽 x : 길이 === 1
+        왼쪽 x : 1번일때
+        오른쪽 x : 마지막일때
+    */
+    if(quizzes.length === 1){
+        addDisplayNone(forwardArrow);
+        addDisplayNone(backArrow);
+    }else if(currentQuiz.quizNum === quizzes.length){
+        addDisplayNone(forwardArrow);
+        removeDisplayNone(backArrow);
+    }else if(currentQuiz.quizNum === 1){
+        addDisplayNone(backArrow);
+        removeDisplayNone(forwardArrow);
+    }else{
+        removeDisplayNone(forwardArrow);
+        removeDisplayNone(backArrow);
+    }
+}
+
+function nextQuiz(){
+    getTheNumOfQuiz(currentQuiz.quizNum+1);
+}
+
+function previousQuiz(){
+    getTheNumOfQuiz(currentQuiz.quizNum-1);
+}
+
 
 function createQuiz(){
     var question = getValue('#question');
     var answers = [];
     answers.push(getValue('#answer-1'));
     answers.push(getValue('#answer-2'));
-    var correctAnswer = getValue('#correctAnswer');
-    var quiz = new Question(question, answers, correctAnswer);
+    var correctAnswer = function(){
+        if(checkAnswerPattern(getValue('#correctAnswer'))){
+            return getValue('#correctAnswer');
+        }else{
+            return undefined;
+        }
+    }
 
-    quizzes.push(quiz);
-    var jsonString = JSON.stringify(quiz);
+    if(correctAnswer() != undefined){
 
-   
-    let storageSize = storage.length;
-
-    storage.setItem(storageSize+1, jsonString);
-    // storage.getItem();
-
-
-    clearNewQuiz();
-    toggleDisplayNone(document.querySelector('#con-1'));
-    toggleActiveBtn(document.querySelector('#btn-1'));
+        var quiz = new Question(question, answers, correctAnswer().trim());
+        
+        quizzes.push(quiz);
+        // var jsonString = JSON.stringify(quiz);
+        // let storageSize = storage.length;
+        // storage.setItem(storageSize+1, jsonString);
+        // storage.getItem();
+        
+        clearNewQuiz();
+    }
 };
 
-function checkPattern(userAnswer){
-    return /[1|2]/.exec(userAnswer);
+function openPrompt(){
+    var userAnswer = (prompt('정답을 입력하세요', ''));
+    if(userAnswer){
+        if(checkAnswerPattern(userAnswer)){
+            currentQuiz.checkTheAnswer(userAnswer);
+        }
+    }
 }
 
-function openPrompt(){
-    var userAnswer = prompt('정답을 입력하세요', '');
-    
-    if(!checkPattern(userAnswer)){
-        alert('숫자 1 또는 2 를 입력하세요.')
+
+function checkAnswerPattern(answer){
+    var regexPattern = new RegExp(/^\s*?[1|2]\s*?$/);
+    if(regexPattern.exec(answer)){
+        return true;
     }else{
-        
+        alert('정답은 숫자 1 또는 2 를 입력하세요.');
+        return false;
     }
+}
+
+function resetRandomQuiz(){
+    setTextContext('.quizNumShow', '');
+    setTextContext('.questionShow', '');
+    setTextContext('.answer1Show', '');
+    setTextContext('.answer2Show', '');
+    setValue('#answerBtn', '정답 입력');
+    removeGreenBtn(document.querySelector('#answerBtn'));
+    removeRedBtn(document.querySelector('#answerBtn'));
+    setAnswerBtn('reset')();
 }
 
 function clearNewQuiz(){
@@ -102,18 +196,28 @@ function getValue(selector){
 };
 
 function setValue(selector, value){
-    document.querySelector(selector).value = value;
+    document.querySelectorAll(selector).forEach(function(el){
+        el.value = value;
+    });
 };
 
 function setTextContext(selector, value){
-    document.querySelector(selector).textContent = value;
-    document.querySelector(selector).style.color = '#555555';
-
+    document.querySelectorAll(selector).forEach(function(el){
+        el.textContent = value;
+        el.style.color = '#555555';
+    });
 }
 
-function setDisplayNone(el){
+function addDisplayNone(el){
     el.classList.add('displayNone');
 };
+
+function removeDisplayNone(el){
+    el.classList.remove('displayNone');
+}
+function removeActiveBtn(el){
+    el.classList.remove('activeBtn');
+}
 
 function toggleDisplayNone(el){
     el.classList.toggle('displayNone');
@@ -123,11 +227,34 @@ function toggleActiveBtn(el){
     el.classList.toggle('activeBtn');
 }
 
+function addGreenBtn(el){
+    el.classList.add('greenBtn');
+}
+
+function removeGreenBtn(el){
+    el.classList.remove('greenBtn');
+}
+
+function addRedBtn(el){
+    el.classList.add('redBtn');
+}
+
+function removeRedBtn(el){
+    el.classList.remove('redBtn');
+}
+
 
 function init(){
 
-   contents.forEach(el => setDisplayNone(el));
-   mainBtns.forEach(el => el.addEventListener('click', function(){
+    removeRedBtn(document.querySelector('#answerBtn'));
+    removeGreenBtn(document.querySelector('#answerBtn'));
+    contents.forEach(el => addDisplayNone(el));
+    mainBtns.forEach(el => el.addEventListener('click', function(){
+        
+        if((el.id ==='btn-2' || el.id === 'btn-3') && quizzes.length === 0 ){
+            return;
+        }
+
         toggleActiveBtn(el);
         if(el.id === 'btn-1'){
             activeBtn = 1;
@@ -136,6 +263,16 @@ function init(){
         }else{
             activeBtn = 3;
         }
-        toggleDisplayNone(document.querySelector('#con-'+activeBtn));
+        for(var i=1; i<4; i++){
+            if(i===activeBtn){
+                toggleDisplayNone(document.querySelector('#con-'+activeBtn));
+            }else{
+                addDisplayNone(document.querySelector('#con-'+i));
+                removeActiveBtn(document.querySelector('#btn-'+i));
+            }
+        }
    }));
+
+   forwardArrow.addEventListener('click', nextQuiz);
+   backArrow.addEventListener('click', previousQuiz);
 };
