c6be687bff7b23c3c08e698567646ceb92989675
Seongjin <seongjinkim1123@gmail.com>
2020-02-06 14:54:40 +0900

    7 challenge : added lecturer's code

--- 7-ES6/challenge1/c1.js
+++ 7-ES6/challenge1/c1.js
@@ -357,3 +357,112 @@ const contorller = (function(){
 
 
 contorller.init();
+
+
+
+
+
+
+
+// answer (lecturer)
+
+// class Element {
+//     constructor(name, buildYear) {
+//         this.name = name;
+//         this.buildYear = buildYear;
+//     }
+// }
+
+
+// class Park extends Element {
+//     constructor(name, buildYear, area, numTrees) {
+//         super(name, buildYear);
+//         this.area = area; //km2
+//         this.numTrees = numTrees;
+//     }
+    
+//     treeDensity() {
+//         const density = this.numTrees / this.area;
+//         console.log(`${this.name} has a tree density of ${density} trees per square km.`);
+//     }
+// }
+
+
+// class Street extends Element {
+//     constructor(name, buildYear, length, size = 3) {
+//         super(name, buildYear);
+//         this.length = length;
+//         this.size = size;
+//     }
+    
+//     classifyStreet () {
+//         const classification = new Map();
+//         classification.set(1, 'tiny');
+//         classification.set(2, 'small');
+//         classification.set(3, 'normal');
+//         classification.set(4, 'big');
+//         classification.set(5, 'huge');
+//         console.log(`${this.name}, build in ${this.buildYear}, is a ${classification.get(this.size)} street.`);
+//     }
+// }
+
+
+// const allParks = [new Park('Green Park', 1987, 0.2, 215),
+//                  new Park('National Park', 1894, 2.9, 3541),
+//                  new Park('Oak Park', 1953, 0.4, 949)];
+
+// const allStreets = [new Street('Ocean Avenue', 1999, 1.1, 4),
+//                    new Street('Evergreen Street', 2008, 2.7, 2),
+//                    new Street('4th Street', 2015, 0.8),
+//                    new Street('Sunset Boulevard', 1982, 2.5, 5)];
+
+
+// function calc(arr) {
+    
+//     const sum = arr.reduce((prev, cur, index) => prev + cur, 0);
+        // reduce() : callback fn 이용                       // initial value : 0 에 모든 요소 값 합한다.
+        // previous value 를 매개변수로 호출 가능
+        
+        // ex) [3, 5, 6].reduce((prev, cur, index) => prev + cur, 0);   // => 뒤의 식 바꿀 수 있음
+        //  -> 0 + 3 = 3   ->   3 + 5 = 8   ->   8 + 6 = 48 
+            // return :  48 
+
+
+//     return [sum, sum / arr.length];
+    
+// }
+
+
+// function reportParks(p) {
+    
+//     console.log('-----PARKS REPORT-----');
+    
+//     // Density
+//     p.forEach(el => el.treeDensity());
+    
+//     // Average age
+//     const ages = p.map(el => new Date().getFullYear() - el.buildYear);
+//     const [totalAge, avgAge] = calc(ages);
+//     console.log(`Our ${p.length} parks have an average of ${avgAge} years.`);
+    
+//     // Which park has more than 1000 trees
+//     const i = p.map(el => el.numTrees).findIndex(el => el >= 1000);
+//     console.log(`${p[i].name} has more than 1000 trees.`);
+    
+// }
+
+
+// function reportStreets(s) {
+    
+//     console.log('-----STREETS REPORT-----');
+    
+//     //Total and average length of the town's streets
+//     const [totalLength, avgLength] = calc(s.map(el => el.length));
+//     console.log(`Our ${s.length} streets have a total length of ${totalLength} km, with an average of ${avgLength} km.`);
+    
+//     // CLassify sizes
+//     s.forEach(el => el.classifyStreet());
+// }
+
+// reportParks(allParks);
+// reportStreets(allStreets);
